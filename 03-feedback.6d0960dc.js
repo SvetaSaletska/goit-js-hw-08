d3ef4147be47e086d732ae7ad90d7a85b8d2c6c6
SvetaSaletska <112178891+SvetaSaletska@users.noreply.github.com>
2023-10-27 22:04:41 +0000

    Deploying to gh-pages from @ SvetaSaletska/goit-js-hw-08@98df4c2f2844e597be9515d8f1d4e6a43850a6b1 🚀

--- 03-feedback.6d0960dc.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t=document.querySelector(".feedback-form");document.querySelector("button");t.elements.message.value=null!==(e=localStorage.getItem(localStorageKey))&&void 0!==e?e:"",t.addEventListener("input",(function(e){localStorage.setItem(localStorageKey,e.target.value)}))}();
-//# sourceMappingURL=03-feedback.6d0960dc.js.map
