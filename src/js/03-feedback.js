dce44eecb97cb43db12dcf576cb527f139b5edfc
SvetaSaletska <svitlanagalevich@gmail.com>
2023-10-27 15:29:24 +0300

    update-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
+const btn = document.querySelector('button');
+
+form.elements.message.value = localStorage.getItem(localStorageKey) ?? "";
+
+form.addEventListener("input", (evt) => {
+  localStorage.setItem(localStorageKey, evt.target.value);
+
+});
+
+// form.addEventListener("submit", (evt) => {
+//   evt.preventDefault();
+//   localStorage.removeItem(localStorageKey);
+//   form.reset();
+// });
 
