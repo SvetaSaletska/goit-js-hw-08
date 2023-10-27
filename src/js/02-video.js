b2e3986dfbfbb04f8ad5cafc80e78f1c9881b2c1
SvetaSaletska <svitlanagalevich@gmail.com>
2023-10-27 14:35:22 +0300

    task-03-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+const LOCAL_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(function(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime.toString());
+}, 1000));
+
+
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY) || 0)
