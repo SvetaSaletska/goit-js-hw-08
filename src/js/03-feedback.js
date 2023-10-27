98df4c2f2844e597be9515d8f1d4e6a43850a6b1
SvetaSaletska <svitlanagalevich@gmail.com>
2023-10-28 01:04:13 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,39 @@
 const form = document.querySelector('.feedback-form');
 const LOCAL_KEY = 'feedback-form-state';
 const btn = document.querySelector('button');
+const throttle = require('lodash.throttle');
 
-form.elements.message.value = localStorage.getItem(localStorageKey) ?? "";
+
+form.addEventListener('input', throttle(handleInput, 500));
+form.addEventListener('submit', handleSubmit);
+
+// form.elements.message.value = localStorage.getItem(LOCAL_KEY) ?? "";
 
 form.addEventListener("input", (evt) => {
-  localStorage.setItem(localStorageKey, evt.target.value);
+const form = evt.target;
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
 
+  // if (email === "" || message === "") {
+  //   return console.log("Please fill in all the fields!");
+  // }
 });
 
-// form.addEventListener("submit", (evt) => {
-//   evt.preventDefault();
-//   localStorage.removeItem(localStorageKey);
-//   form.reset();
+
+
+
+// form.addEventListener("input", (evt) => {
+//   const {
+//     elements: { email, message }
+//   } = evt.currentTarget;
+
+//   localStorage.setItem(LOCAL_KEY, evt.target.value);
+
 // });
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  localStorage.removeItem(LOCAL_KEY);
+  form.reset();
+});
 
